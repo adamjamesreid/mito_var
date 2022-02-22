08d58dc6c8ca89a42ed5aef48bf0bd20b18ca96a
adamjamesreid <ajr236@cam.ac.uk>
2022-02-22 13:56:45 +0000

    Added VCF output for each sample to count_variants.py script

--- count_variants.py
+++ count_variants.py
@@ -6,6 +6,7 @@
 
 import sys
 import gzip
+import subprocess
 
 # Point at which control region begins and from which we will ignore variants until end of sequence
 control_begin = 15120
@@ -33,7 +34,36 @@ def read_vcf (vcf):
             v = x.split('\t')
             yield(v)
 
+def read_vcf_header (vcf):
+    ''' Generater to return header of a VCF file'''
+    for x in read_file(vcf):
+        if x.startswith('#'):
+            yield(x)
+
+def filter_vcf (vcf_file):
+    ''' Generator to return lines of filtered VCF file suitable for rare variant calling'''
+    for x in read_vcf(vcf_file):
+        sample_field = x[-1].split(':')
+        # There can be multiple frequency values where there are multiple alternative alleles
+        freq_vals = sample_field[2].split(',')
+
+        # Skip filtered variants
+        if x[6] != 'PASS':
+            continue
+        # Exclude variants from control region
+        elif int(x[1]) >= control_begin:
+            continue
+        # Exclude variants unless there is a low frequency one
+        min_freq = min(freq_vals)
+        if float(min_freq) > fixed_freq:
+            continue
+        
+        # Assume this is kept in because it hasn't been skipped
+        yield('\t'.join(x))
+        
+
 def count_variants (vcf_file):
+    ''' Generator to return counts of filtered snps and indels suitable for rare variant calling'''
     c = 0
     snps = 0
     indels = 0
@@ -80,3 +110,17 @@ with open(ss, 'r') as s:
             #print(v[-1])
             c, snps, indels = count_variants(v[-1])
             print ("{} {} {} {} {} {} {} {}".format(v[0], v[1], v[2], v[3], v[4], c, snps, indels))
+
+            # Write out filtered VCF
+            out_file = v[-1] + '_filtered.vcf'
+            o = open(out_file, 'w')
+            for i in read_vcf_header(v[-1]):
+                o.write(i)
+            for j in filter_vcf(v[-1]):
+                o.write(j + '\n')
+            o.close()
+            zip_cmd = "bgzip {}".format(v[-1] + '_filtered.vcf')
+            subprocess.call(zip_cmd, shell=True)
+            tabix_cmd = "tabix -p vcf {}".format(v[-1] + '_filtered.vcf.gz')
+            subprocess.call(tabix_cmd, shell=True)
+
