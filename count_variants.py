6d2de60b1e2acd91559277390f28e7ef311fd556
adamjamesreid <ajr236@cam.ac.uk>
2022-03-01 16:16:06 +0000

    Updated mito_var.nf and count_variants.py to allow more flexible variant filtering and force GATK to assess all variant sites

--- count_variants.py
+++ count_variants.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+#################
+# EDITED TO ADJUST FILTERING PARAMTERS EXPERIMENTALLY!!!
+#################
+
 # Count variants from mito_var Nextflow script to determine frequency/type of variants
 # 1. Exclude control region (>15120bp)
 # 2. Include only PASS variants
@@ -7,11 +11,22 @@
 import sys
 import gzip
 import subprocess
+import re
 
 # Point at which control region begins and from which we will ignore variants until end of sequence
 control_begin = 15120
 # Ignore alleles above this frequencyas presumably they were fixed prior to the experiment
-fixed_freq = 0.9
+max_allele_freq = 0.9
+
+# Minimum allele frequency
+min_allele_freq = 1e-4
+
+# Allow these AS_FilterStatus tags through (i.e. call is either PASS or these tags)
+#['base_qual', 'contamination', 'map_qual', 'position', 'strand_bias', 'weak_evidence']
+# n.b. SITE is equivalent to PASS
+#allow_these_tags = ['SITE', 'strand_bias', 'base_qual', 'map_qual', 'weak_evidence']
+allow_these_tags = ['SITE', 'strand_bias']
+
 
 def read_file(fn):
     '''Generator for reading files either zipped or not'''
@@ -40,29 +55,56 @@ def read_vcf_header (vcf):
         if x.startswith('#'):
             yield(x)
 
-def filter_vcf (vcf_file):
+def filter_vcf (vcf_file, control_begin, max_allele_freq, min_allele_freq, allow_these_tags):
     ''' Generator to return lines of filtered VCF file suitable for rare variant calling'''
     for x in read_vcf(vcf_file):
         sample_field = x[-1].split(':')
         # There can be multiple frequency values where there are multiple alternative alleles
         freq_vals = sample_field[2].split(',')
 
-        # Skip filtered variants
-        if x[6] != 'PASS':
-            continue
-        # Exclude variants from control region
-        elif int(x[1]) >= control_begin:
-            continue
-        # Exclude variants unless there is a low frequency one
-        min_freq = min(freq_vals)
-        if float(min_freq) > fixed_freq:
-            continue
-        
-        # Assume this is kept in because it hasn't been skipped
-        yield('\t'.join(x))
+        # Yield variant entry unless it should be filtered
+        if call_filter(x, control_begin, max_allele_freq, min_allele_freq, allow_these_tags):
+            yield('\t'.join(x))
         
 
-def count_variants (vcf_file):
+def call_filter (vcf_line, control_begin, max_allele_freq, min_allele_freq, allow_these_tags):
+    ''' Return True if variant passes filters '''
+    # Exclude variants from control region
+    if int(vcf_line[1]) >= control_begin:
+        #print ("vcf_line[1] in control region")
+        return False
+
+    # Get frequency values for each allele
+    # There can be multiple frequency values where there are multiple alternative alleles
+    sample_field = vcf_line[-1].split(':')
+    freq_vals = sample_field[2].split(',')
+
+    # Look for a set of AS_filter tags which pass
+    m = re.findall(r'AS_FilterStatus=([^;]+)', vcf_line[7]) 
+    # Split status field per allele
+    status_fields = m[0].split('|')
+    # Loop through allele status and frequencies checking whether each passes
+    for s, f in zip(status_fields, freq_vals):
+        passed = True
+        tags = s.split(',')
+        # Loop through each tag to see if it is OK
+        for t in tags:
+            # set passed to false if t is not an allowed tag
+            if t not in allow_these_tags:
+                passed = False
+                #print ("{} has tag {}".format(vcf_line[1], t))
+        # Check frequency
+        if passed:
+            # IF frequnecy is in appropriate range, return True
+            if float(f) >= min_allele_freq and float(f) <= max_allele_freq:
+                return True
+            else:
+                pass
+                #print ("{} has freq {}".format(vcf_line[1], f))
+    # If filters are not passed, return False
+    return False
+
+def count_variants (vcf_file, control_begin, max_allele_freq, min_allele_freq, allow_these_tags):
     ''' Generator to return counts of filtered snps and indels suitable for rare variant calling'''
     c = 0
     snps = 0
@@ -72,27 +114,15 @@ def count_variants (vcf_file):
         # There can be multiple frequency values where there are multiple alternative alleles
         freq_vals = sample_field[2].split(',')
 
-        # Skip filtered variants
-        if x[6] != 'PASS':
-            continue
-        # Exclude variants from control region
-        elif int(x[1]) >= control_begin:
-            continue
-        # Exclude variants unless there is a low frequency one
+        # Check whether this variant passes filters
+        if call_filter(x, control_begin, max_allele_freq, min_allele_freq, allow_these_tags):
+            c += 1
 
-        min_freq = min(freq_vals)
-        if float(min_freq) > fixed_freq:
-            continue
-        
-        # Assume this is kept in because it hasn't been skipped
-        #print(x)
-        c += 1
-
-        # Is it a SNP or indel?
-        if len(x[3]) > 1 or len(x[4]) > 1:
-            indels += 1
-        else:
-            snps += 1
+            # Is it a SNP or indel?
+            if len(x[3]) > 1 or len(x[4]) > 1:
+                indels += 1
+            else: 
+                snps += 1
     return c, snps, indels
 
 ###############
@@ -108,7 +138,7 @@ with open(ss, 'r') as s:
             header = v
         else:
             #print(v[-1])
-            c, snps, indels = count_variants(v[-1])
+            c, snps, indels = count_variants(v[-1], control_begin, max_allele_freq, min_allele_freq, allow_these_tags)
             print ("{} {} {} {} {} {} {} {}".format(v[0], v[1], v[2], v[3], v[4], c, snps, indels))
 
             # Write out filtered VCF
@@ -116,7 +146,7 @@ with open(ss, 'r') as s:
             o = open(out_file, 'w')
             for i in read_vcf_header(v[-1]):
                 o.write(i)
-            for j in filter_vcf(v[-1]):
+            for j in filter_vcf(v[-1], control_begin, max_allele_freq, min_allele_freq, allow_these_tags):
                 o.write(j + '\n')
             o.close()
             zip_cmd = "bgzip {}".format(v[-1] + '_filtered.vcf')
--- mito_var.nf
+++ mito_var.nf
@@ -12,12 +12,25 @@ checkPathParamList = [
 ]
 for (param in checkPathParamList) { if (param) { file(param, checkIfExists: true) } }
 
-// Read in samplesheet and get bams
+// Read in samplesheet and get bams (two channels required)
 Channel
     .fromPath(params.ss, checkIfExists: true)
     .splitCsv(header:true)
     .map{row -> file(row.bam)}
-    .set{ bam_ch }
+    .set{ bam_ch1 }
+
+Channel
+    .fromPath(params.ss, checkIfExists: true)
+    .splitCsv(header:true)
+    .map{row -> file(row.bam)}
+    .set{ bam_ch2 }
+
+// Set up channel of bam indexes for pileup
+Channel
+    .fromPath(params.ss, checkIfExists: true)
+    .splitCsv(header:true)
+    .map{row -> file(row.bam + '.bai', checkIfExists: true)}
+    .set{ bai_ch }
 
 //Set up reference channels
 Channel
@@ -35,24 +48,51 @@ Channel
     .first() // Converts to a value channel to avoid consuming the reference
     .set{dict_ch}
 
+// Run mpileup to call all possible variant sites which GATK can then be forced to consider
+// This allows checking of evidence for every site and allows sites which were otherwise missed through
+process mpileup {
+    publishDir params.outdir, mode:'copy'
+
+    input:
+    path bam from bam_ch1
+    path bai from bai_ch
+    path ref from ref_ch
+    path fai from fai_ch
+
+    output:
+    path "${bam}.mpileup.vcf.gz" into mpileup_vcf_ch
+    path "${bam}.mpileup.vcf.gz.tbi" into mpileup_vcf_tbi_ch
+
+    script:
+    """
+    bcftools mpileup -r chrM -O z -f $ref -o "${bam}.mpileup.vcf.gz" $bam
+    tabix -p vcf "${bam}.mpileup.vcf.gz"
+    """
+
+}
 
 process mutect2_mito {
     publishDir params.outdir, mode:'copy'
 
     input:
-    path bam from bam_ch
+    path mpileup_vcf from mpileup_vcf_ch
+    path mpileup_vcf_tbi from mpileup_vcf_tbi_ch
+    path bam from bam_ch2
     path ref from ref_ch
     path fai from fai_ch
     path dict from dict_ch
 
     output:
-    path "${bam}.vcf.gz" into vcf_ch
-    path "${bam}.vcf.gz.tbi" into vcf_index_ch
-    path "${bam}.vcf.gz.stats" into vcf_stats_ch
+    path "${bam}.mutect2.vcf.gz" into vcf_ch
+    path "${bam}.mutect2.vcf.gz.tbi" into vcf_index_ch
+    path "${bam}.mutect2.vcf.gz.stats" into vcf_stats_ch
 
     script:
     """
-    gatk Mutect2 -R $ref -L chrM --mitochondria-mode -I $bam -O "${bam}.vcf.gz"
+    #gatk Mutect2 -R hg38.fa -L chrM -I MAid261_AK136.md.bam -O MAid261_AK136.md.bam.normalmode.vcf --alleles MAid261_AK136.md.bam.bcftools.vcf.gz
+    #gatk Mutect2 -R $ref -L chrM --mitochondria-mode -I $bam -O "${bam}.vcf.gz"
+    # Add --mitochondria-mode back in ?????
+    gatk Mutect2 -R $ref -L chrM -I $bam -O "${bam}.mutect2.vcf.gz" --alleles $mpileup_vcf
     """
 }
 
